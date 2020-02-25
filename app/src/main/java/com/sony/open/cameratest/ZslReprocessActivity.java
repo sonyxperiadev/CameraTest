0264078daae30446345c45f42b1c2bb36b2d8d31
Sebastian Raase <sebastian.raase@sony.com>
2020-02-25 13:33:30 +0100

    ZslReprocess: fix NonNull annotation build error
    The move from the Android Support Library to Android broke the
    ZslReprocess module. Fix the build error by importing the correct
    NonNull annotation.

--- app/src/main/java/com/sony/open/cameratest/ZslReprocessActivity.java
+++ app/src/main/java/com/sony/open/cameratest/ZslReprocessActivity.java
@@ -27,7 +27,7 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.media.ImageWriter;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
